6d70640a31f38ad83bb653f744ef642198a5a833
Jason Dogariu <j.dogariu@kunzleigh.com>
2018-11-30 11:24:20 -0500

    Ready for presentation

--- Makefile
+++ Makefile
@@ -1,14 +1,17 @@
 CC = cc
 LINKER = -lm
-FLAGS = -Wall -DDEFAULT_MAX="(unsigned long)(1 << 24)"
+FLAGS = -Wall -O3 -DDEFAULT_MAX="(unsigned long)(1 << 26)"
 
-all: aos soa
+all: aos soa malloc
 
 aos:
 	$(CC) $(FLAGS) -o aos-example src/aos-example.c $(LINKER)
 
 soa:
 	$(CC) $(FLAGS) -o soa-example src/soa-example.c $(LINKER)
+
+malloc:
+	$(CC) $(FLAGS) -o malloc-example src/malloc-example.c $(LINKER)
 
 clean: clean-obj
 	rm aos-example soa-example
--- src/malloc-example.c
+++ src/malloc-example.c
@@ -2,8 +2,8 @@
  * Brian Chrzanowski
  * Array of Structures
  *
- * Formal Example code, demonstrating DOD principles with
- * the quadratic formula (-b +- sqrt(b^2 - 4ac)) / 2a
+ * demonstrates how mallocing every item, one at a time is real slow
+ * then the data becomes even more fragmented
  */
 
 #include <stdio.h>
@@ -28,39 +28,49 @@ struct quad_t {
 
 int main(int argc, char **argv)
 {
-	struct quad_t *quads;
+	struct quad_t **quads;
 	int i, total_nans;
 
 	printf("Testing AoS with %ld elements\n", (unsigned long)DEFAULT_MAX);
 
-	quads = malloc(sizeof(struct quad_t) * DEFAULT_MAX); // get some bytes
+	quads = malloc(sizeof(struct quad_t *) * DEFAULT_MAX); // get the outer set of pointers
 	if (!quads) { perror("memory error\n"); exit(1); }
+
+	for (i = 0; i < DEFAULT_MAX; i++) {
+		quads[i] = malloc(sizeof(struct quad_t));
+		if (!quads) { perror("memory error\n"); exit(1); }
+	}
 
 	srand(time(NULL)); // seed the RNG machine
 
 	// set up some trash quadratics
 	for (i = 0; i < DEFAULT_MAX; i++) {
-		quads[i].a = rand() % 100 - 50;
-		quads[i].b = rand() % 100 - 50;
-		quads[i].c = rand() % 100 - 50;
+		quads[i]->a = rand() % 100 - 50;
+		quads[i]->b = rand() % 100 - 50;
+		quads[i]->c = rand() % 100 - 50;
 	}
 
 	// perform the math
 	for (i = 0; i < DEFAULT_MAX; i++) {
-		quads[i].root =
-			(-quads[i].b + sqrt(quads[i].b * quads[i].b - (4 * quads[i].a * quads[i].c)) /
-			 (2 * quads[i].a));
+		quads[i]->root =
+			(-quads[i]->b + sqrt(quads[i]->b * quads[i]->b - (4 * quads[i]->a * quads[i]->c)) /
+			 (2 * quads[i]->a));
 	}
 
 	// print all of the results
 	for (i = 0, total_nans = 0; i < DEFAULT_MAX; i++) {
-		if (quads[i].root != quads[i].root) { // check if it's NAN
+		if (quads[i]->root != quads[i]->root) { // check if it's NAN
 			total_nans++;
 		} 
 	}
 	printf("total nans : %d\n", total_nans);
 
+	for (i = 0; i < DEFAULT_MAX; i++) {
+		free(quads[i]);
+	}
 	free(quads);
 
 	return 0;
 }
+
+
